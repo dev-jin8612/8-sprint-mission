9acfe3174f639685ea097922b7e8ca5a7bc3113e
dev-jin8612 <workspace8612@gmail.com>
2026-04-12 21:57:55 +0900

    미션10 심화 중5

--- src/main/java/com/sprint/mission/discodeit/dto/response/JwtDTO.java
+++ src/main/java/com/sprint/mission/discodeit/dto/response/JwtDTO.java
@@ -1,21 +1,5 @@
 package com.sprint.mission.discodeit.dto.response;
 
 import com.sprint.mission.discodeit.dto.data.UserDto;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class JwtDTO {
-    private UserDto user;
-    private String accessToken;
-
-    @Override
-    public String toString() {
-        return "JwtDTO{" + "user=" + user + ", accessToken='" + accessToken + '\'' + '}';
-    }
-}
+public record JwtDTO(UserDto userDto, String accessToken) {}
